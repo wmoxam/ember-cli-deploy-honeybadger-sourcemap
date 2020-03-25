f505e0b79c11c22a7de501d5d01946111c5c5e90
Wesley Moxam <wes@crowdmark.com>
2020-03-25 11:57:13 -0400

    Fix filename normalization

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ function indexByBaseFilename(files) {
 }
 
 function getBaseFilename(file) {
-  return file.replace(/-[0-9a-f]+\.(js|map)$/, '');
+  return file.replace(/(-[0-9a-f]+)?\.(js|map)$/, '');
 }
 
 function fetchFilePaths(distFiles, basePath, type) {
